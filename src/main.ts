56a816a86262536745d72c29c7cdd7094763e3fa
halconsystems <halcon.org@gmail.com>
2025-09-24 09:23:08 +0500

    registeration solution

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: "https://portal.guardsos.com", 
+    origin: 'https://zany-waffle-6p499pvww47fxqwq-3000.app.github.dev', // Allow all origins in development
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
