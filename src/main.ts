4efd65a5e38b464902542614ae85c5cd51a7c0b5
Jahangeer Ahmed <jahangeer9182@gmail.com>
2025-10-07 05:54:00 +0000

    main.ts edited online with Bitbucket

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*', // just allow everything for now to test
+    origin: ['https://portal.guardsos.com','http://localhost:3000'],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
